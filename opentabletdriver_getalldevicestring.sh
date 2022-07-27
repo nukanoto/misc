bf673c831d7a3569d0b0fd7cafc1127b452a7e9b
nukanoto <nukanoto@nukanoto.net>
2022-07-27 06:40:44 +0000

    fix

--- opentabletdriver_getalldevicestring.sh
+++ opentabletdriver_getalldevicestring.sh
@@ -1,11 +1,11 @@
 otd="otd" # Path to opentabletdriver binary
 
 # VID and PID are USB device id; they can be seen by `lsusb`
-vid=10429
-pid=2309
+vid=
+pid=
 
 for i in {0..1000}; do
-  str="$(otd getstring 10429 2309 $i 2> /dev/null)"
+  str="$(otd getstring ${vid} ${pid} $i 2> /dev/null)"
   res=$?
 
   if [[ $res != 0 ]]; then
